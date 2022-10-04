4b1c4f955f8cedeef8fdfb0585572cbda1eba479
Xartiou <xartiou@gmail.com>
2022-10-04 19:13:52 +0300

    task_1 Less_5

---
+++ Less_5_работа_с_файлами/study_homework/task_1.py
@@ -0,0 +1,12 @@
+"""
+1. Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
+Об окончании ввода данных свидетельствует пустая строка.
+"""
+
+with open('task_1.txt', 'w', encoding='utf-8') as text_file:
+    while True:
+        line = input('Введите текст, для выхода Enter: ')
+        if not line:
+            break
+        text_file.write(line + '\n')
+        # print(line, file=f) # Print в файл
