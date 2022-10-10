79769b39a3be15f60d0fb399f5d402028d17281f
Xartiou <xartiou@gmail.com>
2022-10-10 18:41:49 +0300

    task_3 Less_6_ООП

---
+++ Less_6_ООП/study_homework/task_3.py
@@ -0,0 +1,35 @@
+"""
+3. Реализовать базовый класс Worker (работник),
+в котором определить атрибуты: name, surname, position (должность), income (доход).
+Последний атрибут должен быть защищенным и ссылаться на словарь,
+содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}.
+Создать класс Position (должность) на базе класса Worker.
+В классе Position реализовать методы
+получения полного имени сотрудника (get_full_name) и
+дохода с учетом премии (get_total_income).
+Проверить работу примера на реальных данных
+(создать экземпляры класса Position, передать данные, проверить значения атрибутов,
+вызвать методы экземпляров).
+"""
+
+
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+    def get_full_name(self):
+        return f"{self.name} {self.surname}"
+
+    def get_total_income(self):
+        return f"{sum(self._income.values())}"
+
+
+cucumber = Position("Semen", "Ogurtcov", "vegetable", 25000, 7000)
+print(cucumber.position)
+print(cucumber.get_full_name())
+print(cucumber.get_total_income())
